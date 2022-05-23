5c847ca77dce0247047659c9093b473bc1bebe81
Alex Salnikov <50184325+Denvormine@users.noreply.github.com>
2022-05-23 22:20:30 +0300

    Initial commit

---
+++ TripletProject/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Concurrent;
+using TripletProject;
+
+var watch = System.Diagnostics.Stopwatch.StartNew();
+
+string filepath = args[0];
+
+if (!File.Exists(filepath))
+{
+    Console.WriteLine("File doesn't exist");
+}
+
+ConcurrentDictionary<Triplet, int> tripletDictionary = 
+    new ConcurrentDictionary<Triplet, int>();
+
+Parallel.ForEach(File.ReadLines(filepath), line =>
+{
+    for (int i = 2; i < line.Length;)
+    {
+        Triplet triplet = new Triplet(line[i - 2], line[i - 1], line[i]);
+    
+        if (!Char.IsLetter(triplet.Letter3))
+        {
+            i += 3;
+            continue;
+        }
+    
+        if (!Char.IsLetter(triplet.Letter2))
+        {
+            i += 2;
+            continue;
+        }
+    
+        if (!Char.IsLetter(triplet.Letter1))
+        {
+            i++;
+            continue;
+        }
+        
+        tripletDictionary.AddOrUpdate(triplet, 1, (_, i1) => i1 + 1);
+        i++;
+    }
+});
+
+foreach (var keyValuePair in tripletDictionary.OrderByDescending(x => x.Value).Take(10))
+{
+    Console.WriteLine(keyValuePair.Key + " " + keyValuePair.Value);
+}
+
+watch.Stop();
+
+Console.WriteLine(watch.ElapsedMilliseconds);
---
+++ TripletProject/Triplet.cs
@@ -0,0 +1,9 @@
+﻿namespace TripletProject;
+
+public record struct Triplet(char Letter1, char Letter2, char Letter3)
+{
+    public override string ToString()
+    {
+        return "" + Letter1 + Letter2 + Letter3;
+    }
+}
